c6ce95acba766fb31ab12ae0abad70748f0b0835
sega-hsj <hsj970319@163.com>
2022-03-29 11:17:42 +0800

    Update referit3d_net.py

--- referit3d/models/referit3d_net.py
+++ referit3d/models/referit3d_net.py
@@ -24,42 +24,57 @@ class ReferIt3DNet_transformer(nn.Module):
     def __init__(self,
                  args,
                  n_obj_classes,
-                 class_name_tokens):
+                 class_name_tokens,
+                 ignore_index):
 
         super().__init__()
 
-        self.inner_dim = args.inner_dim
-        self.rotate_number = args.rotate_number
+        self.bert_path = args.bert_pretrain_path
+
         self.view_number = args.view_number
+        self.rotate_number = args.rotate_number
+
         self.label_lang_sup = args.label_lang_sup
         self.aggregate_type = args.aggregate_type
+
+        self.encoder_layer_num = args.encoder_layer_num
+        self.decoder_layer_num = args.decoder_layer_num
+        self.decoder_nhead_num = args.decoder_nhead_num
+
+        self.object_dim = args.object_latent_dim
+        self.inner_dim = args.inner_dim
         
-        self.drop_rate = 0.15
+        self.dropout_rate = args.dropout_rate
+        self.lang_cls_alpha = args.lang_cls_alpha
+        self.obj_cls_alpha = args.obj_cls_alpha
 
         self.object_encoder = PointNetPP(sa_n_points=[32, 16, None],
                                         sa_n_samples=[[32], [32], [None]],
                                         sa_radii=[[0.2], [0.4], [None]],
                                         sa_mlps=[[[3, 64, 64, 128]],
                                                 [[128, 128, 128, 256]],
-                                                [[256, 256, 512, 512]]])
+                                                [[256, 256, self.object_dim, self.object_dim]]])
 
-        encoder_layer_num = args.encoder_layer_num
-        self.language_encoder = BertModel.from_pretrained(args.bert_pretrain_path)
-        self.language_encoder.encoder.layer = BertModel(BertConfig()).encoder.layer[:encoder_layer_num]
+        self.language_encoder = BertModel.from_pretrained(self.bert_pretrain_path)
+        self.language_encoder.encoder.layer = BertModel(BertConfig()).encoder.layer[:self.encoder_layer_num]
 
-        decoder_layer_num = args.decoder_layer_num
-        decoder_nhead_num = args.decoder_nhead_num
-        self.refer_encoder = nn.TransformerDecoder(torch.nn.TransformerDecoderLayer(d_model=self.inner_dim, nhead=decoder_nhead_num, dim_feedforward=2048, activation="gelu"), num_layers=decoder_layer_num)
+        self.refer_encoder = nn.TransformerDecoder(torch.nn.TransformerDecoderLayer(d_model=self.inner_dim, 
+            nhead=self.decoder_nhead_num, dim_feedforward=2048, activation="gelu"), num_layers=self.decoder_layer_num)
 
         # Classifier heads
-        self.language_clf = nn.Sequential(nn.Linear(self.inner_dim, self.inner_dim), nn.ReLU(), nn.Dropout(self.drop_rate), nn.Linear(self.inner_dim, n_obj_classes))
-        self.object_language_clf = nn.Sequential(nn.Linear(self.inner_dim, self.inner_dim), nn.ReLU(), nn.Dropout(self.drop_rate), nn.Linear(self.inner_dim, 1))
+        self.language_clf = nn.Sequential(nn.Linear(self.inner_dim, self.inner_dim), 
+                                        nn.ReLU(), nn.Dropout(self.dropout_rate), 
+                                        nn.Linear(self.inner_dim, n_obj_classes))
+
+        self.object_language_clf = nn.Sequential(nn.Linear(self.inner_dim, self.inner_dim), 
+                                                nn.ReLU(), nn.Dropout(self.dropout_rate), 
+                                                nn.Linear(self.inner_dim, 1))
 
         if not self.label_lang_sup:
-            self.obj_clf = MLP(self.inner_dim, [512, 512, n_obj_classes], dropout_rate=self.drop_rate)
+            self.obj_clf = MLP(self.inner_dim, [self.object_dim, self.object_dim, n_obj_classes], dropout_rate=self.dropout_rate)
 
         self.obj_feature_mapping = nn.Sequential(
-            nn.Linear(512, self.inner_dim),
+            nn.Linear(self.object_dim, self.inner_dim),
             nn.LayerNorm(self.inner_dim),
         )
 
@@ -72,7 +87,7 @@ class ReferIt3DNet_transformer(nn.Module):
 
         self.logit_loss = nn.CrossEntropyLoss()
         self.lang_logits_loss = nn.CrossEntropyLoss()
-        self.class_logits_loss = nn.CrossEntropyLoss(ignore_index=524)
+        self.class_logits_loss = nn.CrossEntropyLoss(ignore_index=ignore_index)
 
     @torch.no_grad()
     def aug_input(self, input_points, box_infos):
@@ -83,6 +98,8 @@ class ReferIt3DNet_transformer(nn.Module):
         B,N,P = xyz.shape[:3]
         rotate_theta_arr = torch.Tensor([i*2.0*np.pi/self.rotate_number for i in range(self.rotate_number)]).to(self.device)
         view_theta_arr = torch.Tensor([i*2.0*np.pi/self.view_number for i in range(self.view_number)]).to(self.device)
+        
+        # rotation
         if self.training:
             # theta = torch.rand(1) * 2 * np.pi  # random direction rotate aug
             theta = rotate_theta_arr[torch.randint(0,self.rotate_number,(B,))]  # 4 direction rotate aug
@@ -96,6 +113,8 @@ class ReferIt3DNet_transformer(nn.Module):
 
             input_points[:, :, :, :3] = torch.matmul(xyz.reshape(B,N*P,3), rotate_matrix).reshape(B,N,P,3)
             bxyz = torch.matmul(bxyz.reshape(B,N,3), rotate_matrix).reshape(B,N,3)
+        
+        # multi-view
         bsize = box_infos[:,:,-1:]
         boxs=[]
         for theta in view_theta_arr:
@@ -111,7 +130,7 @@ class ReferIt3DNet_transformer(nn.Module):
         referential_loss = self.logit_loss(LOGITS, batch['target_pos'])
         obj_clf_loss = self.class_logits_loss(CLASS_LOGITS.transpose(2, 1), batch['class_labels'])
         lang_clf_loss = self.lang_logits_loss(LANG_LOGITS, batch['target_class'])
-        total_loss = referential_loss + 0.5 * obj_clf_loss + 0.5 * lang_clf_loss
+        total_loss = referential_loss + self.obj_cls_alpha * obj_clf_loss + self.lang_cls_alpha * lang_clf_loss
         return total_loss
 
     def forward(self, batch: dict, epoch=None):
@@ -131,7 +150,7 @@ class ReferIt3DNet_transformer(nn.Module):
         ## obj_encoding
         obj_feats = self.obj_feature_mapping(objects_features)
         box_infos = self.box_feature_mapping(boxs)
-        obj_infos = obj_feats[:, None].repeat(1,self.view_number,1,1) + box_infos
+        obj_infos = obj_feats[:, None].repeat(1, self.view_number, 1, 1) + box_infos
 
         # <LOSS>: obj_cls
         if self.label_lang_sup:
@@ -149,9 +168,9 @@ class ReferIt3DNet_transformer(nn.Module):
         LANG_LOGITS = self.language_clf(lang_infos[:,0])
         
         ## multi-modal_fusion
-        cat_infos = obj_infos.reshape(B * self.view_number, -1, self.inner_dim)
-        mem_infos = lang_infos[:, None].repeat(1, self.view_number, 1, 1).reshape(B * self.view_number, -1, self.inner_dim)
-        out_feats = self.refer_encoder(cat_infos.transpose(0, 1), mem_infos.transpose(0, 1)) .transpose(0, 1).reshape(B, self.view_number, -1, self.inner_dim)
+        cat_infos = obj_infos.reshape(B*self.view_number, -1, self.inner_dim)
+        mem_infos = lang_infos[:, None].repeat(1, self.view_number, 1, 1).reshape(B*self.view_number, -1, self.inner_dim)
+        out_feats = self.refer_encoder(cat_infos.transpose(0, 1), mem_infos.transpose(0, 1)).transpose(0, 1).reshape(B, self.view_number, -1, self.inner_dim)
 
         ## view_aggregation
         refer_feat = out_feats
